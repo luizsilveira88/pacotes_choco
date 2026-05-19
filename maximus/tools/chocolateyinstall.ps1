513d466fd461b3ffa565614218f7bad2f115d3de
Luiz Felipe Silveira <luiz_silveira88@hotmail.com>
2026-05-19 18:54:50 -0300

    Inclui Maximus v7.6.0

--- maximus/tools/chocolateyinstall.ps1
+++ maximus/tools/chocolateyinstall.ps1
@@ -25,7 +25,7 @@ $installerName = "$packageId-$packageVersion$installerExtension"
 $toolsDir     = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $networkPath  = "\\179.97.96.73\repositorio$\installers\$packageId\$installerName"
 $localExePath = Join-Path $toolsDir $installerName
-. "$toolsDir\helpers.ps1"
+. "$toolsDir\helpers-1.0.0.ps1"
 
 # =====================================================
 # REMOÇÃO DE LEGADOS (SE NECESSARIO)
